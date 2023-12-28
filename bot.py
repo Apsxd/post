d2e819a757ad81df27a61212d676ad4ff879e7f7
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-28 19:49:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telethon import TelegramClient, events
 from decouple import config
 import logging
 from telethon.sessions import StringSession
+import os
 
 # Configure logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
@@ -13,11 +14,17 @@ print("Starting...")
 APP_ID = config("APP_ID", default=0, cast=int)
 API_HASH = config("API_HASH", default=None, cast=str)
 SESSION = config("SESSION", default="", cast=str)
-FROM_CHANNEL = config("FROM_CHANNEL", default="", cast=str)
-TO_CHANNEL = config("TO_CHANNEL", default="", cast=str)
+FROM_ = config("FROM_CHANNEL", default="", cast=str)
+TO_ = config("TO_CHANNEL", default="", cast=str)
 
-FROM = [int(i) for i in FROM_CHANNEL.split(',')]
-TO = [int(i) for i in TO_CHANNEL.split(',')]
+BLOCKED_TEXTS = config("BLOCKED_TEXTS", default="", cast=lambda x: [i.strip().lower() for i in x.split(',')])
+MEDIA_FORWARD_RESPONSE = config("MEDIA_FORWARD_RESPONSE", default="yes").lower()
+
+FROM = [int(i) for i in FROM_.split()]
+TO = [int(i) for i in TO_.split()]
+
+YOUR_ADMIN_USER_ID = config("YOUR_ADMIN_USER_ID", default=0, cast=int)
+BOT_API_KEY = config("BOT_API_KEY", default="", cast=str)
 
 # Initialize Telethon client
 try:
@@ -34,11 +41,20 @@ async def sender_bH(event):
         try:
             message_text = event.raw_text.lower()
 
-            # Add your additional logic here, if needed
+            if any(blocked_text in message_text for blocked_text in BLOCKED_TEXTS):
+                print(f"Blocked message containing one of the specified texts: {event.raw_text}")
+                logging.warning(f"Blocked message containing one of the specified texts: {event.raw_text}")
+                continue
 
             if event.media:
+                user_response = MEDIA_FORWARD_RESPONSE
+                if user_response != 'yes':
+                    print(f"Media forwarding skipped by user for message: {event.raw_text}")
+                    continue
+
                 await steallootdealUser.send_message(i, message_text, file=event.media)
                 print(f"Forwarded media message to channel {i}")
+
             else:
                 await steallootdealUser.send_message(i, message_text)
                 print(f"Forwarded text message to channel {i}")
@@ -48,7 +64,7 @@ async def sender_bH(event):
 
 # Event handler for old messages
 async def forward_old_messages():
-    destination_channels = TO  # Assuming TO_CHANNEL contains a comma-separated list of channel IDs
+    destination_channels = TO  # Assuming TO contains a list of channel IDs
     for channel_id in destination_channels:
         try:
             destination_channel = await steallootdealUser.get_entity(int(channel_id))
